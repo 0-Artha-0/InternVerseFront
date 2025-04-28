e0f283fec5f50f21236402062d629750027107e9
artha <U21101466@sharjah.ac.ae>
2025-04-28 19:58:21 +0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.secret_key = os.environ.get("SESSION_SECRET", "default-dev-secret-key")
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)  # needed for url_for to generate with https
 
 # Backend API configuration
-app.config['BACKEND_API_URL'] = os.environ.get('BACKEND_API_URL', 'http://localhost:8000')
+app.config['BACKEND_API_URL'] = os.environ.get('BACKEND_API_URL', 'thepromptocrats-hackathon-stg-wa-uaen-02-cnaffvfcbbf0fsak.uaenorth-01.azurewebsites.net')
 
 # Import routes
 import routes
